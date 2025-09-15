9d13f6f76cc861f70c40cd8f83d330e64c96f701
Elmidor Mario Rodriguez zagalo <shoote.io@elmidorgroup.com>
2025-09-15 11:24:03 +0100

    effet official
    mete script

---
+++ assets/js/particles.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const particles = document.querySelector(".particles");
+  for (let i = 0; i < 20; i++) {
+    const p = document.createElement("div");
+    p.classList.add("particle");
+    p.style.width = `${Math.random()*6+4}px`;
+    p.style.height = p.style.width;
+    p.style.left = `${Math.random()*100}%`;
+    p.style.animationDuration = `${10 + Math.random()*20}s`;
+    p.style.animationDelay = `${Math.random()*10}s`;
+    particles.appendChild(p);
+  }
+});
